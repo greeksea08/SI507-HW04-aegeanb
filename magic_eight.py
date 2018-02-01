46654314c985cf6f06844ee24db8a9654b591a29
Aegean Buenafe <aegean@Aegeans-MacBook-Pro.local>
2018-01-31 21:10:45 -0500

    added check_question

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,21 @@
 def question():
 	ask = input("What is your question?")
 	return ask
+
+
+
+
+
+
+
+
+
+ask_prompt = ""
+while ask_prompt != "quit":
+	ask_prompt = question()
+	if ask_prompt.endswith("?"):
+		print(random.choice(possible_answers))
+	elif ask_prompt == "quit":
+		print("END")
+	else:
+		print("I’m sorry, I can only answer questions(hint:?),try again or type 'quit' to exit.")
